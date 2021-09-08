74655fd6cbce2f2dbcd175f0d59fcf6df4ba6e77
Romi Pane <rmpane@gmail.com>
2021-09-08 22:36:25 +0700

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace CommandAPI.Models.Models
+{
+    public class Command{
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string HowToUse { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        public string CommandLine { get; set; }
+    }    
+}
